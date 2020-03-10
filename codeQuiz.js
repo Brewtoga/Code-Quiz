a92ebe3e5c8588e7cdeccc784edefb45302b4d94
lets travel <george@brewsystems.com>
2020-03-10 12:30:11 -0400

    I was able to use hide and unhide to get some good things working

--- codeQuiz.js
+++ codeQuiz.js
@@ -1,9 +1,77 @@
 $(document).ready(function () {
-    
+
     $("#startButton").on("click", startQuiz)
     function startQuiz() {
+        var i = 0; i++;
+        var currentHighscore = 500;
+        var codeQuestions = [
+            {
+                question: "Inside which HTML element do we put the JavaScript?",
+                answerA: 'js',
+                answerB: 'javascripting',
+                answerC: 'scripting',
+                answerD: 'script',
+                correctA: 'script',
+            },
+            // {
+            //     question: 'Inside which HTML element do we put the JavaScript?',
+            //     answerA: 'js',
+            //     answerB: 'javascripting',
+            //     answerC: 'scripting',
+            //     answerD: 'script',
+            //     correctA: 'script',
+            // },
+            // {
+            //     question: 'Inside which HTML element do we put the JavaScript?',
+            //     answerA: 'js',
+            //     answerB: 'javascripting',
+            //     answerC: 'scripting',
+            //     answerD: 'script',
+            //     correctA: 'script',
+            // },
+            // {
+            //     question: 'Inside which HTML element do we put the JavaScript?',
+            //     answerA: 'js',
+            //     answerB: 'javascripting',
+            //     answerC: 'scripting',
+            //     answerD: 'script',
+            //     correctA: 'script',
+            // },
+            // {
+            //     question: 'Inside which HTML element do we put the JavaScript?',
+            //     answerA: 'js',
+            //     answerB: 'javascripting',
+            //     answerC: 'scripting',
+            //     answerD: 'script',
+            //     correctA: 'script',
+            // },
+            // {
+            //     question: 'Inside which HTML element do we put the JavaScript?',
+            //     answerA: 'js',
+            //     answerB: 'javascripting',
+            //     answerC: 'scripting',
+            //     answerD: 'script',
+            //     correctA: 'script',
+            // },
+        ];
         console.log("start");
-        $("#startButton").attr("class","hide");
+        console.log(codeQuestions);
+        console.log(codeQuestions[0].question);
+
+        $("#startButton").attr("class", "hide");
+        $("#highscore").append(" "+currentHighscore);
+        $("#question").attr("class", "unhide col-6");$("#question").text(codeQuestions[0].question);
+        // $("#ans1").attr("class", "unhide col-6 btn btn-info btn-hover btn-pill");$("#question").text(codeQuestions[0].answerA);
+        // $("#ans2").attr("class", "unhide col-6 btn btn-info btn-hover btn-pill");$("#question").text(codeQuestions[0].answerB);
+        // $("#ans3").attr("class", "unhide col-6 btn btn-info btn-hover btn-pill");$("#question").text(codeQuestions[0].answerC);
+        $("#secandMInDisplay").text("Min  and  Sec");
+
+
+
+
+
+        
 
     }
+
 });
