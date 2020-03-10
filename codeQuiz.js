a0ff23c012872c9fdbe0918788b49bb999e7ddb8
lets travel <george@brewsystems.com>
2020-03-10 13:39:20 -0400

    added event listenersand function to check answers

--- codeQuiz.js
+++ codeQuiz.js
@@ -13,14 +13,14 @@ $(document).ready(function () {
                 answerD: 'script',
                 correctA: 'script',
             },
-            // {
-            //     question: 'Inside which HTML element do we put the JavaScript?',
-            //     answerA: 'js',
-            //     answerB: 'javascripting',
-            //     answerC: 'scripting',
-            //     answerD: 'script',
-            //     correctA: 'script',
-            // },
+            {
+                question: 'What is the second Question?',
+                answerA: 'A',
+                answerB: 'B',
+                answerC: 'C',
+                answerD: 'D',
+                correctA: 'C',
+            },
             // {
             //     question: 'Inside which HTML element do we put the JavaScript?',
             //     answerA: 'js',
@@ -59,18 +59,33 @@ $(document).ready(function () {
         console.log(codeQuestions[0].question);
 
         $("#startButton").attr("class", "hide");
-        $("#highscore").append(" "+currentHighscore);
-        $("#question").attr("class", "unhide col-6");$("#question").text(codeQuestions[0].question);
-        // $("#ans1").attr("class", "unhide col-6 btn btn-info btn-hover btn-pill");$("#question").text(codeQuestions[0].answerA);
-        // $("#ans2").attr("class", "unhide col-6 btn btn-info btn-hover btn-pill");$("#question").text(codeQuestions[0].answerB);
-        // $("#ans3").attr("class", "unhide col-6 btn btn-info btn-hover btn-pill");$("#question").text(codeQuestions[0].answerC);
-        $("#secandMInDisplay").text("Min  and  Sec");
+        $("#highscore").append(" " + currentHighscore);
+        $("#question").attr("class", "unhide"); $("#question").text(codeQuestions[0].question);
+        $("#ans1").attr("class", "unhide btn btn-info btn-hover btn-pill"); $("#ans1").text(codeQuestions[0].answerA);
+        $("#ans2").attr("class", "unhide btn btn-info btn-hover btn-pill"); $("#ans2").text(codeQuestions[0].answerB);
+        $("#ans3").attr("class", "unhide btn btn-info btn-hover btn-pill"); $("#ans3").text(codeQuestions[0].answerC);
+        $("#ans4").attr("class", "unhide btn btn-info btn-hover btn-pill"); $("#ans4").text(codeQuestions[0].answerD);
+        var minutesAndSeconds = "Minutes and Seconds"
+        $("#secandMin").attr("class", "unhide text-center"); $("#secandMin").text(minutesAndSeconds);
+
+
+        $("#ans1").on("click", checkAnswer);
+        $("#ans2").on("click", checkAnswer);
+        $("#ans3").on("click", checkAnswer);
+        $("#ans4").on("click", checkAnswer);
+
+
+        function checkAnswer() {
+            console.log("in checkanswer function")
+            $("#secandMin").text("getting closer");
+            
+        }
+
+            
 
 
 
 
-
-        
 
     }
 
